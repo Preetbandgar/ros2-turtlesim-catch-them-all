c6419fcee0ad48e3316acc35dccc54d4f8d925fe
Pritam Bandgar <157994717+Preetbandgar@users.noreply.github.com>
2026-01-01 01:22:05 +0530

    updated turtle_controller.py & turtle_spawner.py

--- src/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_controller.py
+++ src/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_controller.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 import math
 import rclpy
+from functools import partial
 from rclpy.node import Node
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
 from my_robot_interfaces.msg import Turtle
 from my_robot_interfaces.msg import TurtleArray
+from my_robot_interfaces.srv import CatchTurtle
 
 class TurtleControllerNode(Node):
     def __init__(self):
         super().__init__("turtle_controller")
         self.turtle_to_catch_: Turtle = None
+        self.catch_closest_turtle_ = True
         self.pose_ : Pose = None
         self.cmd_vel_publisher = self.create_publisher(
             Twist, "turtle1/cmd_vel", 10)
@@ -18,6 +21,7 @@ class TurtleControllerNode(Node):
             Pose, "turtle1/pose", self.pose_callback, 10)
         self.alive_turtles_subscriber = self.create_subscription(
             TurtleArray, "alive_turtles", self.callback_alive_turtles, 10)
+        self.catch_turtle_client = self.create_client(CatchTurtle, "catch_turtle")
         self.control_loop_timer = self.create_timer(
             0.01, self.control_loop)
          
@@ -27,7 +31,21 @@ class TurtleControllerNode(Node):
 
     def callback_alive_turtles(self, msg: TurtleArray):
         if len(msg.turtles) > 0:
-            self.turtle_to_catch_ = msg.turtles[0]
+            if self.catch_closest_turtle_:
+                closest_turtle = None
+                closest_turtle_distance = None
+
+                for turtle in msg.turtles:
+                    dist_x = turtle.x - self.pose_.x
+                    dist_y = turtle.y - self.pose_.y
+                    distance = math.sqrt(dist_x**2 + dist_y**2)
+
+                    if closest_turtle == None or distance < closest_turtle_distance:
+                        closest_turtle = turtle
+                        closest_turtle_distance = distance
+                self.turtle_to_catch_ = closest_turtle
+            else:
+                self.turtle_to_catch_ = msg.turtles[0]
 
     def control_loop(self):
         if self.pose_ == None or self.turtle_to_catch_ == None:
@@ -57,10 +75,27 @@ class TurtleControllerNode(Node):
            #target reached
             cmd.linear.x = 0.0
             cmd.angular.z = 0.0
+            self.call_catch_turtle_service(self.turtle_to_catch_.name)
+            self.turtle_to_catch_ = None
         
         self.cmd_vel_publisher.publish(cmd)
 
 
+    def call_catch_turtle_service(self, turtle_name):
+        while not self.catch_turtle_client.wait_for_service(1.0):
+            self.get_logger().warn("Waiting for catch turtle service...")
+        
+        request = CatchTurtle.Request()
+        request.name = turtle_name
+
+        future = self.catch_turtle_client.call_async(request)
+        future.add_done_callback(
+            partial(self.callback_call_catch_turtle_service, turtle_name=turtle_name))
+        
+    def callback_call_catch_turtle_service(self, future, turtle_name):
+        response : CatchTurtle.Response = future.result()
+        if not response.success:
+            self.get_logger().error("Turtle " + turtle_name + " could not be removed.")
 
 def main(args=None):
     rclpy.init(args=args)
--- src/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_spawner.py
+++ src/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_spawner.py
@@ -5,8 +5,10 @@ from functools import partial
 import random
 import math
 from turtlesim.srv import Spawn
+from turtlesim.srv import Kill
 from my_robot_interfaces.msg import Turtle
 from my_robot_interfaces.msg import TurtleArray
+from my_robot_interfaces.srv import CatchTurtle
 
 class TurtleSpawnerNode(Node):
     def __init__(self):
@@ -17,7 +19,17 @@ class TurtleSpawnerNode(Node):
         self.alive_turtles_publisher_ = self.create_publisher(
             TurtleArray, "alive_turtles", 10)
         self.spawn_client_ = self.create_client(Spawn, "/spawn")
-        self.spawn_turtle_timer_ = self.create_timer(2.0, self.spawn_new_turtle)
+        self.kill_client_ = self.create_client(Kill, "/kill")
+        self.catch_turtle_service_ = self.create_service(
+            CatchTurtle, "catch_turtle", self.callback_catch_turtle)
+        self.spawn_turtle_timer_ = self.create_timer(0.8, self.spawn_new_turtle)
+
+    def callback_catch_turtle(self, request: CatchTurtle.Request,
+                             response: CatchTurtle.Response):
+        self.call_kill_service(request.name)
+        # call kill service
+        response.success = True
+        return response
 
     def publish_alive_turtles(self):
         msg = TurtleArray()
@@ -57,6 +69,25 @@ class TurtleSpawnerNode(Node):
             new_turtle.theta = request.theta
             self.alive_turtles_.append(new_turtle)
             self.publish_alive_turtles()
+
+    def call_kill_service(self, turtle_name):
+        while not self.kill_client_.wait_for_service(1.0):
+            self.get_logger().warn("Waiting for kill service...")
+        
+        request = Kill.Request()
+        request.name = turtle_name
+
+        future = self.kill_client_.call_async(request)
+        future.add_done_callback(
+            partial(self.callback_call_kill_service, turtle_name=turtle_name))
+        
+    def callback_call_kill_service(self, future, turtle_name):
+        for (i, turtle) in enumerate(self.alive_turtles_):
+            if turtle.name == turtle_name:
+               del self.alive_turtles_[i]
+               self.publish_alive_turtles()
+               break
+
 
 def main(args=None):
     rclpy.init(args=args)
